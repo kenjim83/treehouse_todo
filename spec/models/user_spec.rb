6f41dc6132244be2a1a35a0400f16269b6da9841
Kenji Miwa <kenjimiwa@Kenjis-MacBook-Pro.local>
2014-07-10 17:42:15 -0700

    user attr rspecs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,19 +23,29 @@ describe User do
       expect(user).to validate_uniqueness_of(:email)
     end
 
+    it "requires a unique email (case insensitive)" do
+      user.email = "KMIWA@HOTMAIL.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+  end
+
+  context "#downcase_email" do
+    it "makes the email attribute lowercase" do
+      user = User.new(valid_attributes.merge(email: "KMIWA@HOTMAIL.COM"))
+      # user.downcase_email
+      # expect(user.email).to eq("kmiwa@hotmail.com")
+      expect{ user.downcase_email }.to change{ user.email }
+        .from("KMIWA@HOTMAIL.COM")
+        .to("kmiwa@hotmail.com")
+    end
+
     it "downcases an email before saving" do
       user = User.new(valid_attributes)
       user.email = "KMIWA@HOTMAIL.COM"
       expect(user.save).to be_true
       expect(user.email).to eq("kmiwa@hotmail.com")
     end
-  end
 
-  context "#downcase_email" do
-    it "makes the email attribute lowercase" do
-      user = User.new(valid_attributes.merge(email: "KMIWA@HOTMAIL.COM"))
-      user.downcase_email
-      expect(user.email).to be eq("kmiwa@hotmail.com")
-    end
   end
 end
